e8f96e48a484d0333bd87d167f3b8df8862e477d
HuyHoang <htranngochuy@gmail.com>
2025-11-06 20:07:02 +0700

    Add files via upload

--- merge_sort.cpp
+++ merge_sort.cpp
@@ -1,24 +1,28 @@
 
 #include <iostream>
-int temp[1000];
+int t[1000];
 
+void in (int a[], int n) {
+    for (int i = 0; i < n; i++)
+        std::cout << a[i] << " ";
+    std::cout << "\n";
+}
 void mergeParts(int a[], int l, int m, int r) {
     int i = l, j = m + 1;
     int k = 0;
 
     while (i <= m && j <= r) {
-        int nextValue;
+        int l;
 
-        if (a[i] < a[j]) nextValue = a[i++];
-        else nextValue = a[j++];
-
-        temp[k++] = nextValue;
+        if (a[i] < a[j]) l = a[i++];
+        else l = a[j++];
+        t[k++] = l;
     }
 
-    while (i <= m) temp[k++] = a[i++];
-    while (j <= r) temp[k++] = a[j++];
+    while (i <= m) t[k++] = a[i++];
+    while (j <= r) t[k++] = a[j++];
 
-    for (int i = 0; i < k; i++) a[l + i] = temp[i];
+    for (int i = 0; i < k; i++) a[l + i] = t[i];
 
 }
 
@@ -29,7 +33,6 @@ void mergeSort(int a[], int l, int r) {
 
     mergeSort(a, l, m);
     mergeSort(a, m + 1, r);
-
     mergeParts(a, l, m, r);
 }
 int main()
@@ -37,8 +40,7 @@ int main()
 	int a[] = { 3, 10, 1, 23, 103, 34, 17, 53, 64, 71 };
 	int n = sizeof(a) / sizeof(a[0]);
 	mergeSort(a, 0, n-1);
-    for (int i=0; i < n; i++)
-		std::cout << a[i] << " ";
+    in(a, n);
 	return 0;
 }
 
